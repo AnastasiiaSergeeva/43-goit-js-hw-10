86fed4ddc8616ea18ec5d583fd92d5867f295ea6
AnastasiiaSergeeva <73837165+AnastasiiaSergeeva@users.noreply.github.com>
2022-04-21 16:28:36 +0200

    parcel

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import './css/styles.css';
+
+const DEBOUNCE_DELAY = 300;
+const inputRef = document.querySelector('#search-box');
+const countryList = document.querySelector('.country-list');
+const countryInfo = document.querySelector('.country-info');
+
+fetchCountries(name)
+
